29962b478e666b8b40f5bd5e8467189d3041b4af
SameerAliSk <sameeraligkl@gmail.com>
2024-02-19 16:26:29 +0530

    little lemon project

---
+++ src/components/Form.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import "./styles/ReservationsContent.css";
+import {useForm} from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup";
+
+const schema = yup.object({
+    name: yup.string().required("Full name is a required field!"),
+    email: yup.string().required("Email is a required field!").email("Email is not valid!"),
+    telephone: yup.string().required("Telephone is a required field!").matches(/^(\+\d{2,3}\s)?\(?\d{3}\)?[\s.-]\d{2}[\s.-]\d{3}[\s.-]\d{4}$/, "Phone number must match the form 233 00 000 0000"),
+    guests: yup.number().min(1, "There must be at least 1 guest!").required("Please specify number of guests per table!"),
+    date: yup.date()
+    .min(new Date(), 'Date must be today or later') // Ensures date is today or later
+    .required('Date and time are required')
+    .when('dateTime', (dateTime, schema) => {
+      // Ensures that the date and time is after current moment
+      if (dateTime) {
+        return schema.min(new Date(), 'Date and time must be in the future');
+      }
+    })
+})
+
+function Form() {
+
+
+    const { handleSubmit, register, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
+
+    console.log(errors)
+
+    const formSubmit = (data) => {
+        console.table(data)
+    }
+
+    return (
+        <form onSubmit={handleSubmit(formSubmit)}>
+            <fieldset>
+                <div className="field">
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" placeholder="John Doe" name="name" {...register("name")} />
+                    <span className="error-message">{errors.name?.message}</span>
+                </div>
+                <div className="field">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" placeholder="text@email.com" name="email" {...register("email")}/>
+                    <span className="error-message">{errors.email?.message}</span>
+                </div>
+                <div className="field">
+                    <label htmlFor="telephone">Telephone</label>
+                    <input type="tel" placeholder="233 00 000 0000" name="telephone" {...register("telephone")}/>
+                    <span className="error-message">{errors.telephone?.message}</span>
+                </div>
+
+                {/*<div className="guestsdate">*/}
+                <div className="field occasion">
+                    <label htmlFor="occasion">Occasion (optional)</label>
+                    <div className="options">
+                        <select name="occasion" {...register("occasion")}>
+                            <option value="select">Select occasion</option>
+                            <option value="birthday">Birthday</option>
+                            <option value="engagement">Engagement</option>
+                            <option value="anniversary">Anniversary</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="field guest">
+                    <label htmlFor="guests">Guests</label>
+                    <input type="number" placeholder="2" name="guests" {...register("guests")}/> 
+                    <span className="error-message">{errors.guests?.message}</span>
+                </div>
+                {/*</div>*/}
+                        
+                <div className="field">
+                    <label htmlFor="date">Date & Time</label>
+                    <input type="datetime-local" name="date" {...register("date")} />
+                    <span className="error-message">{errors.date?.message}</span>
+                </div>
+                <button className="reserve-btn" type="submit">Reserve</button>
+            </fieldset>
+        </form>
+  )
+}
+
+export default Form
